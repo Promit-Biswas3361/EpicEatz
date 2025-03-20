983bc15934a23e2b65c91e8fdbd9d8cee70d1377
Promit Biswas <promit.biswas0710@gmail.com>
2025-03-21 04:42:47 +0530

    Bug fixes

--- src/Frontend/New-partner/Step1.jsx
+++ src/Frontend/New-partner/Step1.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavbarPartner from "./NavbarPartner";
-import FooterPartner from "./FooterPartner";
 import { ConciergeBell, ClipboardList, ShieldCheck } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
@@ -11,9 +10,12 @@ const Step1 = () => {
     <div className="bg-gray-100">
       <NavbarPartner />
 
-      <form className="mt-16.5 md:mt-21.5 pt-10 pb-30 w-full">
+      <form
+        action={() => navigate("/new-partner/step2")}
+        className="mt-16.5 md:mt-21.5 pt-10 pb-30 w-full"
+      >
         <div className="flex flex-row justify-center items-start px-10 lg:px-25 xl:px-45 2xl:px-55">
-          <div className="hidden md:block flex-[35%] mr-3.5 bg-white h-fit rounded-lg pt-3">
+          <div className="hidden md:block md:flex-[35%] mr-3.5 bg-white h-fit rounded-lg pt-3">
             <div className="border-b-1 border-gray-200 px-3 pb-4 mb-3">
               <h4 className="text-xl font-semibold">
                 Complete your registration
@@ -39,7 +41,7 @@ const Step1 = () => {
             </div>
           </div>
 
-          <div className="flex-[65%] ml-3.5">
+          <div className="flex-1 md:flex-[65%] ml-3.5">
             <h1 className="text-4xl font-bold mb-8">Restaurant Information</h1>
 
             <div className="bg-white rounded-lg mb-8 py-6 px-">
@@ -84,7 +86,7 @@ const Step1 = () => {
                 </div>
                 <div className="flex">
                   <div className="flex items-center rounded-lg border-1 border-gray-200 w-25 justify-center mr-1.5 shadow-sm">
-                    <img src="/india.png" alt="" className="h-6 w-auto" />
+                    <img src="/india.png" className="h-6 w-auto mr-1.5" />
                     <p>+91</p>
                   </div>
                   <input
@@ -150,12 +152,11 @@ const Step1 = () => {
 
         <footer className="w-full bg-white fixed bottom-0 py-5 px-4 z-20">
           <div className="flex justify-end">
-            <button
+            <input
+              type="submit"
+              value="Next"
               className="bg-[#F22400] rounded-xl px-7 py-2 text-white font-semibold cursor-pointer"
-              onClick={() => navigate("/new-partner/step2")}
-            >
-              Next
-            </button>
+            />
           </div>
         </footer>
       </form>
--- src/Frontend/New-partner/Step2.jsx
+++ src/Frontend/New-partner/Step2.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import NavbarPartner from "./NavbarPartner";
-import FooterPartner from "./FooterPartner";
 
 const Step2 = () => {
   return (
     <div className="bg-gray-100 h-screen">
       <NavbarPartner />
-      <FooterPartner text="Submit" url="/" />
     </div>
   );
 };
